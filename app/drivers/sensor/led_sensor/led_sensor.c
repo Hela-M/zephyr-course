d77ea3c3e6c351f8f39295020f90fa9082a7f8a4
Hela-M <enovo@Hela.localdomain>
2026-06-28 23:14:27 +0100

    l6-task2: add custom API to LED sensor driver

--- app/drivers/sensor/led_sensor/led_sensor.c
+++ app/drivers/sensor/led_sensor/led_sensor.c
@@ -1,106 +1,132 @@
-#include <errno.h>
-#include <zephyr/device.h>
 #include <zephyr/drivers/gpio.h>
-#include <zephyr/drivers/sensor.h>
-#include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/sys/util.h>
+#include "led_sensor.h"
 
 LOG_MODULE_REGISTER(led_sensor, LOG_LEVEL_INF);
 
 #define DT_DRV_COMPAT hela_led_sensor
 
 struct led_sensor_config {
-	struct gpio_dt_spec led;
+    struct gpio_dt_spec led;
 };
-
 
 struct led_sensor_data {
-	bool state;
+    bool    led_is_on;
+    int32_t runtime_parameter;
 };
 
+
 static int led_sensor_sample_fetch(const struct device *dev,
-				   enum sensor_channel chan)
+                                   enum sensor_channel chan)
 {
-	const struct led_sensor_config *cfg = dev->config;
-	struct led_sensor_data *data = dev->data;
-	int err;
+    const struct led_sensor_config *config = dev->config;
+    struct led_sensor_data         *data   = dev->data;
+    int ret;
 
-	ARG_UNUSED(chan);
+    ret = gpio_pin_set_dt(&config->led, 1);
+    if (ret < 0) {
+        LOG_ERR("sensor_sample_fetch: failed to set LED pin (%d)", ret);
+        return ret;
+    }
 
-	err = gpio_pin_set_dt(&cfg->led, 1);
-	if (err) {
-		return err;
-	}
+    data->led_is_on = true;
+    LOG_INF("sensor_sample_fetch: LED ON, runtime_parameter=%d",
+            data->runtime_parameter);
 
-	data->state = true;
-
-	LOG_INF("Sample fetched (LED ON)");
-
-	return 0;
+    return 0;
 }
 
+
 static int led_sensor_channel_get(const struct device *dev,
-				  enum sensor_channel chan,
-				  struct sensor_value *val)
+                                  enum sensor_channel chan,
+                                  struct sensor_value *val)
 {
-	const struct led_sensor_config *cfg = dev->config;
-	struct led_sensor_data *data = dev->data;
-	int err;
+    const struct led_sensor_config *config = dev->config;
+    struct led_sensor_data         *data   = dev->data;
+    int ret;
 
-	if (chan != SENSOR_CHAN_ALL) {
-		return -ENOTSUP;
-	}
+    if (!data->led_is_on) {
+        if (val != NULL) {
+            val->val1 = data->runtime_parameter;
+            val->val2 = 0;
+        }
+        LOG_INF("sensor_channel_get: LED already OFF, runtime_parameter=%d",
+                data->runtime_parameter);
+        return 0;
+    }
 
-	if (val) {
-		val->val1 = data->state ? 1 : 0;
-		val->val2 = 0;
-	}
+    ret = gpio_pin_set_dt(&config->led, 0);
+    if (ret < 0) {
+        LOG_ERR("sensor_channel_get: failed to clear LED pin (%d)", ret);
+        return ret;
+    }
 
-	err = gpio_pin_set_dt(&cfg->led, 0);
-	if (err) {
-		return err;
-	}
+    data->led_is_on = false;
 
-	data->state = false;
+    if (val != NULL) {
+        val->val1 = data->runtime_parameter;
+        val->val2 = 0;
+    }
 
-	LOG_INF("Channel read (LED OFF)");
+    LOG_INF("sensor_channel_get: LED OFF, runtime_parameter=%d",
+            data->runtime_parameter);
 
-	return 0;
+    return 0;
+}
+
+
+static int led_sensor_set_runtime_parameter_impl(const struct device *dev,
+                                                 int32_t runtime_parameter)
+{
+    struct led_sensor_data *data = dev->data;
+
+    data->runtime_parameter = runtime_parameter;
+    LOG_INF("custom API: runtime_parameter changed to %d",
+            data->runtime_parameter);
+
+    return 0;
 }
 
 static int led_sensor_init(const struct device *dev)
 {
-	const struct led_sensor_config *cfg = dev->config;
+    const struct led_sensor_config *config = dev->config;
+    struct led_sensor_data         *data   = dev->data;
 
-	if (!gpio_is_ready_dt(&cfg->led)) {
-		LOG_ERR("LED GPIO not ready");
-		return -ENODEV;
-	}
+    if (!gpio_is_ready_dt(&config->led)) {
+        LOG_ERR("LED GPIO device is not ready");
+        return -ENODEV;
+    }
 
-	return gpio_pin_configure_dt(&cfg->led, GPIO_OUTPUT_INACTIVE);
+    data->runtime_parameter = 0;
+    data->led_is_on         = false;
+
+    return gpio_pin_configure_dt(&config->led, GPIO_OUTPUT_INACTIVE);
 }
 
-static const struct sensor_driver_api led_sensor_api = {
-	.sample_fetch = led_sensor_sample_fetch,
-	.channel_get = led_sensor_channel_get,
+
+static const struct led_sensor_driver_api led_sensor_api = {
+    .sensor_api = {
+        .sample_fetch = led_sensor_sample_fetch,
+        .channel_get  = led_sensor_channel_get,
+    },
+    .set_runtime_parameter = led_sensor_set_runtime_parameter_impl,
 };
 
-
-#define LED_SENSOR_DEFINE(inst)                             \
-static struct led_sensor_data led_data_##inst;              \
-                                                            \
-static const struct led_sensor_config led_cfg_##inst = {    \
-    .led = GPIO_DT_SPEC_INST_GET(inst, led_gpios),          \
-};                                                          \
-                                                            \
-DEVICE_DT_INST_DEFINE(inst,                                 \
-        led_sensor_init,                                    \
-        NULL,                                               \
-        &led_data_##inst,                                   \
-        &led_cfg_##inst,                                    \
-        POST_KERNEL,                                        \
-        CONFIG_SENSOR_INIT_PRIORITY,                        \
-        &led_sensor_api);
+#define LED_SENSOR_DEFINE(inst)                                             \
+    static struct led_sensor_data led_sensor_data_##inst;                   \
+                                                                            \
+    static const struct led_sensor_config led_sensor_config_##inst = {      \
+        .led = GPIO_DT_SPEC_INST_GET(inst, led_gpios),                          \
+    };                                                                      \
+                                                                            \
+    SENSOR_DEVICE_DT_INST_DEFINE(inst,                                      \
+                                 led_sensor_init,                           \
+                                 NULL,                                      \
+                                 &led_sensor_data_##inst,                   \
+                                 &led_sensor_config_##inst,                 \
+                                 POST_KERNEL,                               \
+                                 CONFIG_SENSOR_INIT_PRIORITY,               \
+                                 &led_sensor_api);
 
 DT_INST_FOREACH_STATUS_OKAY(LED_SENSOR_DEFINE)
